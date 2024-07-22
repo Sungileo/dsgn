89590f7abb65372767c6674b7753296b375cdb62
Sungileo <112994103+Sungileo@users.noreply.github.com>
2024-07-23 08:53:53 +0900

    ㅋ

--- 자바_알고리즘_연습문제/src/q7_ptrint_atoz.java
+++
@@ -1,9 +0,0 @@
-package 자바_알고리즘_연습문제.src;
-
-public class q7_ptrint_atoz {
-    public static void main(String[] args) {
-        for (int i = 65; i < 91; i++) {
-            System.out.println((char)i);
-        }
-    }
-}
